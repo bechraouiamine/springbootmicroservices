e7f00f77dd4e05e97d5b039d7b0214ff24fa4d1c
Bechraoui <abechraoui@monceauassurances.com>
2020-10-22 14:46:14 +0200

    Events and States enum

---
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderEventEnum.java
@@ -0,0 +1,9 @@
+package guru.sfg.beer.order.service.web.model;
+
+/**
+ * Created by BECHRAOUI, 22/10/2020
+ */
+public enum BeerOrderEventEnum {
+    VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED, ALLOCATION_SUCCESS,
+    ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED, BEERORDER_PICKED_UP
+}
