3849debd8f71ec7f4b49f79e79736900fcf0728f
Bechraoui <abechraoui@monceauassurances.com>
2020-10-22 14:54:38 +0200

    State Machine Config initialization

---
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/statemachine/BeerOrderStateMachineConfig.java
@@ -0,0 +1,30 @@
+package guru.sfg.beer.order.service.statemachine;
+
+import guru.sfg.beer.order.service.web.model.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.web.model.BeerOrderStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+/**
+ * Created by BECHRAOUI, 22/10/2020
+ */
+@Configuration
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.PICKED_UP)
+                .end(BeerOrderStatusEnum.DELIVRED)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+    }
+}
--- mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderStatusEnum.java
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderStatusEnum.java
@@ -17,7 +17,7 @@
 
 package guru.sfg.beer.order.service.web.model;
 
-public enum OrderStatusEnum {
+public enum BeerOrderStatusEnum {
     NEW, VALIDATED, VALIDATION_EXCEPTION, ALLOCATED, ALLOCATION_EXCEPTION, PENDING_INVENTORY,
-    PICKED_UP, DELIVRED, DELIVERY_EXCEPTION
+    PICKED_UP, DELIVRED, DELIVERY_EXCEPTION, READY
 }
