b470dd579c5784fa35d1b5bdd37bf07d61666f0b
bechraoui amine <bechraoui.amine@gmail.com>
2020-12-15 18:07:13 +0100

    Initial commit for spring gateway + config google profile

---
+++ mssc-brewery-gateway/src/main/java/guru/sfg/beer/brewery/gateway/MsscBreweryGatewayApplication.java
@@ -0,0 +1,16 @@
+package guru.sfg.beer.brewery.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aminebechraoui, on 15/12/2020, in guru.sfg.beer.brewery.gateway
+ */
+@SpringBootApplication
+public class MsscBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryGatewayApplication.class, args);
+	}
+
+}
---
+++ mssc-brewery-gateway/src/test/java/guru/sfg/beer/brewery/gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.sfg.beer.brewery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by aminebechraoui, on 15/12/2020, in guru.sfg.beer.brewery.gateway
+ */
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
