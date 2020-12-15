0b8d6aafc0f322cd6ba9abf2d538b84ac3fb7de2
bechraoui amine <bechraoui.amine@gmail.com>
2020-12-15 18:29:51 +0100

    Route gateway config + filters

--- mssc-brewery-gateway/src/main/java/guru/sfg/beer/brewery/gateway/config/GoogleConfig.java
+++ mssc-brewery-gateway/src/main/java/guru/sfg/beer/brewery/gateway/config/GoogleConfig.java
@@ -18,7 +18,8 @@ public class GoogleConfig {
     public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                .route(r -> r.path("/googlesearch")
+                .route(r -> r.path("/googlesearch2")
+                        .filters(f-> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
                         .uri("https://google.com")
                         .id("google"))
                 .build();
